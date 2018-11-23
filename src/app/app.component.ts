93ecc740eff5ad6c38d34da00596d5aceda16937
Richard Scott <richard1.scott@nottscc.gov.uk>
2018-11-23 20:00:59 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from "@angular/core";
+import { Validators } from "@angular/forms";
+import { FieldConfig } from "./field.interface";
+import { DynamicFormComponent } from "./components/dynamic-form/dynamic-form.component";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+  regConfig: FieldConfig[] = [];
+  submit(value: any) {}
+}
